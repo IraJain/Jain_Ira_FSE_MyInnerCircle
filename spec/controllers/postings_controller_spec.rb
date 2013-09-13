f2464439f140bf7c94f8698ba537c8f4c48a07e0
ira jain <ira.jain@sv.cmu.edu>
2013-09-13 16:37:28 -0700

    changed few test cases

--- spec/controllers/postings_controller_spec.rb
+++ spec/controllers/postings_controller_spec.rb
@@ -7,75 +7,115 @@ describe PostingsController do
     @user = FactoryGirl.create(:user)
     sign_in @user
 
-
-   # visit root_path
-    #fill_in "SignIn_email",    with: user.email
-    #fill_in "SignIn_password", with: user.password
-   # click_button "Sign in"
-
   end
 
-  def valid_attributes
-    {title: "helloji", description: "this is desc"}
+  def table_data_post
+    {title: "testing title", description: "this is desc for testing post controller"}
   end
 
-  def valid_session
-  #   { "warden.user.user.key" => session["warden.user.user.key"] }
-  end
-
+  describe "GET new" do
     it "assigns a new posting as @posting" do
       get :new, {}
       assigns(:posting).should be_a_new(Posting)
     end
+  end
 
   describe "GET edit" do
-    it "assigns the requested micropost as @micropost" do
-      dummy = subject.current_user.inspect
-      posting = Posting.create! valid_attributes
-
-
+    it "assigns the requested posting as @posting" do
+     posting = Posting.create! table_data_post
       get :edit, {:id => posting.to_param}
       assigns(:posting).should eq(posting)
     end
   end
 
+  describe "POST create" do
 
-=begin
-  describe "GET index" do
-    it "assigns all postings" do
-      posting = FactoryGirl.create(:posting)
-      get :index
-      expect(assigns(:postings)).to eq([posting])
-    end
+    describe "with valid params" do
+      it "creates a new Posting" do
+        expect {
+          post :create, {:posting => table_data_post}
+        }.to change(Posting, :count).by(1)
+      end
 
-    it "renders the index template" do
-      get :index  ,:format => "html"
-      response.should render_template(:index)
+      it "assigns a newly created posting as @posting" do
+        post :create, {:posting => table_data_post}
+        assigns(:posting).should be_a(Posting)
+        assigns(:posting).should be_persisted
+      end
 
-    end
-  end
-=end
-
-=begin
-
-    describe "Get new" do
-      it "should get a new post" do
-        get :new
-        assigns(:posting).should be_a_new(Posting)
+      it "redirects to the created posting" do
+        post :create, {:posting => table_data_post}
+        response.should redirect_to(home_index_path)
       end
     end
 
- describe "show" do
+    describe "with invalid params" do
+      it "assigns a newly created but unsaved posting as @posting" do
+        # Trigger the behavior that occurs when invalid params are submitted
+        Posting.any_instance.stub(:save).and_return(false)
+        post :create, {:posting => {}}
+        assigns(:posting).should be_a_new(Posting)
+      end
 
-      let(:posting) {FactoryGirl.create(:posting) }
+    end
+  end
 
-   it "should find the posting by its id" do
-     get :show, :id => :posting.id
-     assigns[:posting].title.should == "test title posting "
-   end
- end
-=end
+  describe "GET show" do
+    it "assigns the requested posting as @posting" do
+      posting = Posting.create! table_data_post
+      get :show, {:id => posting.to_param}
+      assigns(:posting).should eq(posting)
+    end
+  end
 
+  describe "PUT update" do
+
+    describe "with valid params" do
+      it "updates the requested posting" do
+        posting = Posting.create! table_data_post
+        Posting.any_instance.should_receive(:update_attributes).with({'title' => 'updated title'})
+        put :update, {:id => posting.to_param, :posting => {'title' => 'updated title'}}
+      end
+
+      it "assigns the requested posting as @posting" do
+        posting = Posting.create! table_data_post
+        put :update, {:id => posting.to_param, :posting => table_data_post}
+        assigns(:posting).should eq(posting)
+      end
+
+      it "redirects to the posting index" do
+        posting = Posting.create! table_data_post
+        put :update, {:id => posting.to_param, :posting => table_data_post}
+        response.should redirect_to(home_index_path)
+      end
+    end
+
+    describe "with invalid params" do
+      it "assigns the posting as @posting" do
+        posting = Posting.create! table_data_post
+        # Trigger the behavior that occurs when invalid params are submitted
+        Posting.any_instance.stub(:save).and_return(false)
+        put :update, {:id => posting.to_param, :posting => {}}
+        assigns(:posting).should eq(posting)
+      end
+
+    end
+  end
+
+  describe "DELETE destroy" do
+    it "destroys the requested posting" do
+      posting = Posting.create! table_data_post
+      expect {
+        delete :destroy, {:id => posting.to_param}
+      }.to change(Posting, :count).by(-1)
+    end
+
+    it "redirects to the postings list" do
+      posting = Posting.create! table_data_post
+      delete :destroy, {:id => posting.to_param}
+      response.should redirect_to(home_index_path)
+    end
+  end
 
 
 end
